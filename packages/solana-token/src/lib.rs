a2c3e16a24d588e4554c50537727f9d05807007f
hackartists <hackartists@gmail.com>
2025-02-19 07:49:39 +0900

    feat(workflow): update dioxus-cli installation and add solana-token package

---
+++ packages/solana-token/src/lib.rs
@@ -0,0 +1,19 @@
+use spl_token_2022::{
+    error::TokenError,
+    processor::Processor,
+    solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey},
+};
+
+solana_program::entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if let Err(error) = Processor::process(program_id, accounts, instruction_data) {
+        // catch the error so we can print it
+        error.print::<TokenError>();
+        return Err(error);
+    }
+    Ok(())
+}
